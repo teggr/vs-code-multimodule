44436778cd782c7c3884c443e6d8ceba6356d1a6
Robin Tegg <robin@tegg.me.uk>
2018-01-07 19:04:01 +0000

    initial commit

---
+++ vs-code-core/src/main/java/com/robintegg/blog/Utility.java
@@ -0,0 +1,13 @@
+package com.robintegg.blog;
+
+/**
+ * Hello world!
+ *
+ */
+public class Utility 
+{
+    public static void sayHello( String name )
+    {
+        System.out.println( "Hello "+ name +"!" );
+    }
+}
---
+++ vs-code-web/src/main/java/com/robintegg/blog/App.java
@@ -0,0 +1,13 @@
+package com.robintegg.blog;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Utility.sayHello("robin");
+    }
+}
